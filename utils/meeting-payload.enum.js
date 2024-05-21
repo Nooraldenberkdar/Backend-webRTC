9cd1c891751501dd6a8f2ec3859acff7a80e5863
nooralden <nooraldenberkdar80@gmail.com>
2024-05-21 20:48:26 +0300

    first commit

---
+++ utils/meeting-payload.enum.js
@@ -0,0 +1,22 @@
+const MeetingPayloadEnum={
+    JOIN_MEETING:'join-meeting',
+    JOINED_MEETING:'joined-meeting',
+    USER_JOINED:'user-joined',
+    CONNECTION_REQUEST:'connection-request',
+    INCOMMING_CONNECTION_REQUEST:'incomming-connection-request',
+    OFFER_SDB:'offer-sdb',
+    ANSWER_SDB:'answer-sdb',
+    LEAVE_MEETING:'leave-meeting',
+    END_MEETING:'end-meeting',
+    USER_LEFT:'user-left',
+    MEETING_ENDED:'meeting-ended',
+    ICECANDIDATE:'icecandidate',
+    VIDEO_TOGGLE:'video-toggle',
+    AUDIO_TOGGLE:'audio-toggle',
+    NOT_FOUND:'not-found',
+    UNKNOWN:'unknown'
+}
+
+module.exports  ={
+    MeetingPayloadEnum
+}
